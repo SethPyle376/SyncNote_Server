4d2a0473fdc98ca9c524b5f35897a75f7a64d31a
SethPyle376 <pyl13002@byui.edu>
2017-07-05 20:59:17 -0600

    Account System
    -Users now have their own accounts specific to their gmail
    -Users have access to only their own notes and notes shared with them
    -Users can share notes with other users by specifying their gmail
    address.

--- server2.js
+++ server2.js
@@ -6,16 +6,25 @@
 
 var net = require('net');
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var sockets = [];
 var currentText = "This is the current server message\n";
 
-mongoose.connect('mongodb://localhost/SyncNoteTest1');
+var Note = new Schema({
+	title: String,
+	note: String,
+	users: [{type: String}]
+});
+mongoose.model("Note", Note);
 
-var noteModel = mongoose.model('Note', { title: String, note: String} );
+var noteModel = mongoose.model('note', Note);
+var firstNote = new noteModel({title: "test", data: "it works", users: ["Seth", "Test"]});
 
-var firstNote = new noteModel({title: "This is the first note", note: "Congratz"});
-var nextNote = new noteModel({title: "This is the second test note", note: "ayeee"});
 
+mongoose.connect('mongodb://localhost/SyncNoteTest2');
+
+//REFERENCE
+/*
 firstNote.save(function(err) {
 	if (err) {
 		console.log(err);
@@ -25,14 +34,31 @@ firstNote.save(function(err) {
 	}
 })
 
-nextNote.save(function(err) {
+noteModel.findOne({users: "Seth"}, function(err, noteObj) {
+			if (err) {
+				console.log(err);
+				//Message error to user
+			}
+			else if (noteObj) {
+				console.log("Found");
+				//Broadcast here
+			}
+			else {
+				console.log("Note not found");
+			}
+		});
+
+noteModel.findOneAndUpdate({users: "Seth"}, {$push: {users: "Test2"}}, {new: true}, function(err, note) {
 	if (err) {
-		console.log(err);
+		console.log("Error writing to array");
 	}
-	else {
-		console.log("Saved first note successfully");
+	else
+	{
+		console.log(note.users);
 	}
-})
+	}
+);
+*/
 
 var svr = net.createServer(function(sock) {
 
@@ -45,150 +71,125 @@ var svr = net.createServer(function(sock) {
 	}
 })
 
-    console.log('Connected: ' + sock.remoteAddress + ':' + sock.remotePort);
-    sockets.push(sock);
+console.log('Connected: ' + sock.remoteAddress + ':' + sock.remotePort);
+sockets.push(sock);
 
-    var data2 = sock.remoteAddress + ":" + sock.remotePort + '\n';
+sock.on('data', function(data) {
+	console.log(data.toString());
 
-    sock.on('data', function(data) {
-    	console.log(data.toString());
+	var jsonObject;
+	try {
+		jsonObject = JSON.parse(data);
+		var command = Object.keys(jsonObject)[0];
+		console.log(jsonObject.command);
+	} catch (e) {
+		console.log("error parsing json data: " + data);
+		jsonObject = {};
+		jsonObject.command = "error";
+		jsonObject.data = "";
+		jsonObject.target = "";
+	}
 
-    	var jsonObject;
-    	try {
-			jsonObject = JSON.parse(data);
-			var command = Object.keys(jsonObject)[0];
-			console.log(jsonObject.command);
-		} catch (e) {
-			console.log("error parsing json data: " + data);
-			jsonObject = {};
-			jsonObject.command = "error";
-			jsonObject.data = "";
-			jsonObject.target = "";
+
+	if (jsonObject.command == "login") {
+		sock.email = jsonObject.target;
+		console.log(sock.email + " has logged in.");
+	}
+
+	else if (jsonObject.command == "create") {
+		var newNote = noteModel({title: jsonObject.target, note: jsonObject.data, users: [sock.email]});
+		newNote.save(function(err) {
+				console.log(err);
+				//Message user of error.
+			});
+	}
+
+	else if (jsonObject.command == "addUser") {
+		noteModel.findOneAndUpdate({title: jsonObject.target}, {$push: {users: jsonObject.data}}, {new: true}, function(err, note) {
+		if (err) {
+			console.log("Error writing to array");
 		}
-	
-		if (jsonObject.command == "command") {
-			if (jsonObject.command == "pull")
-			{
+		else
+		{
+			console.log(note.users);
+		}
+	})
+	//TODO Send email notification
+
+	}
+
+	else if (jsonObject.command == "push") {
+		noteModel.findOne({title: jsonObject.target}, function(err, noteObj) {
+			if (err) {
+				console.log(err);
+				//Message error to user
+			}
+			else if (noteObj) {
+				noteObj.note = jsonObject.data;
+				noteObj.save();
+				console.log(noteObj.data);
+				//Broadcast here
+			}
+			else {
+				var newNote = noteModel({title: jsonObject.target, note: jsonObject.data, users: [sock.email]});
+				newNote.save(function(err) {
+				console.log(err);
+				//Message user of error.
+				});
+				console.log("Note not found, creating");
+			}
+				});
+	}	
+	else if (jsonObject.command == "pull") {
+		noteModel.findOne({title : jsonObject.target}, function(err, noteObj) {
+			if (err) {
+				console.log(err);
+			}
+			else if (noteObj) {
 				var jsonVariable = {};
-				jsonVariable["command"] = "data";
-				jsonVariable["data"] = currentText;
+				jsonVariable["command"] = "update";
+				jsonVariable["data"] = noteObj.note;
+				console.log(noteObj.data);
 				jsonVariable = JSON.stringify(jsonVariable);
 				jsonVariable += '\n';
-				console.log(jsonVariable);
-				console.log(currentText);
 				sock.write(jsonVariable);
 			}
-		}
-		else if (jsonObject.command == "create") {
-			noteModel.findOne({title: jsonVariable["target"]}, function(err, userObj)
-			{
-				if (err) {
-					console.log(err);
-					//Message user of error
-				}
-				else if (userObj)
-				{
-					//Message user that note is already created.
-				}
-				else
-				{
-					var newNote = noteModel({title: jsonVariable["target"]});
-					newNote.save(function(err) {
-						console.log(err);
-						//Message user of error.
-					})
-				}
-
-			});
-
-		}
-
-		else if (jsonObject.command == "push") {
-			noteModel.findOne({title: jsonObject.target}, function(err, noteObj) {
-				if (err) {
-					console.log(err);
-					//Message error to user
-				}
-				else if (noteObj) {
-					noteObj.note = jsonObject.data;
-					noteObj.save();
-					//Broadcast here
-				}
-				else {
-					console.log("Note not found, creating");
-
-					noteModel.findOne({title: jsonObject.target}, function(err, userObj)
-					{
-						if (err) {
-							console.log(err);
-						}
-						else if (userObj)
-						{
-
-						}
-						else
-						{
-							var newNote = noteModel({title: jsonObject.target, data: jsonObject.data});
-							console.log("Created new note");
-							newNote.save(function(err) {
-							console.log(err);
-						})
-					}
-
-					});
-				}	
+			else {
+				console.log(err);
+				//Message error to client
+			}
+		})
+	}
+	else if (jsonObject.command == "list") {
+		console.log("List command received");
+		noteModel.find({users: sock.email}, function(err, list) {
+			list.forEach(function(note) {
+				console.log("Sending one to list");
+				var jsonVariable = {};
+				jsonVariable["command"] = "list";
+				jsonVariable["data"] = note.title;
+				var sending = JSON.stringify(jsonVariable);
+				sending += '\n';
+				console.log(sending);
+				sock.write(sending);
 			})
-		}
+		})
 
-		else if (jsonObject.command == "pull") {
-			noteModel.findOne({title : jsonObject.target}, function(err, noteObj) {
-				if (err) {
-					console.log(err);
-				}
-				else if (noteObj) {
-					var jsonVariable = {};
-					jsonVariable["command"] = "update";
-					jsonVariable["data"] = noteObj.note;
-					jsonVariable = JSON.stringify(jsonVariable);
-					jsonVariable += '\n';
-					sock.write(jsonVariable);
-				}
-				else {
-					console.log(err);
-					//Message error to client
-				}
-			})
-		}
-		else if (jsonObject.command == "list") {
-			console.log("List command received");
-			noteModel.find({}, function(err, list) {
-				list.forEach(function(note) {
-					console.log("Sending one to list");
-					var jsonVariable = {};
-					jsonVariable["command"] = "list";
-					jsonVariable["data"] = note.title;
-					var sending = JSON.stringify(jsonVariable);
-					sending += '\n';
-					console.log(sending);
-					sock.write(sending);
-				})
-			})
+	}
 
-		}
+	});
 
-    });
-
-    sock.on('end', function() {
-        console.log('Disconnected: ' + sock.remoteAddress + ':' + sock.remotePort);
-        var idx = sockets.indexOf(sock);
-        if (idx != -1) {
-            delete sockets[idx];
-        }
-    });
+	sock.on('end', function() {
+	    console.log('Disconnected: ' + sock.remoteAddress + ':' + sock.remotePort);
+	    var idx = sockets.indexOf(sock);
+	    if (idx != -1) {
+	        delete sockets[idx];
+	    }
+	});
 });
 
 var svraddr = 'localhost';
-var svrport = 4000;
+var svrport = 5000;
 
-svr.listen(4000);
+svr.listen(6000);
 console.log('Server Created at ' + svraddr + ':' + svrport + '\n');
